1b3f7b744738007aa3f233e2980620fd4f2e5e27
galaxy <867718012@qq.com>
2019-08-22 15:41:33 +0800

    审计1

---
+++ 代码审计/files/index.php
@@ -0,0 +1,19 @@
+ <?php
+highlight_file('flag.php');
+$_GET['id'] = urldecode($_GET['id']);
+$flag = 'flag{kun_ye_niu_bi}';
+if (isset($_GET['uname']) and isset($_POST['passwd'])) {
+    if ($_GET['uname'] == $_POST['passwd'])
+
+        print 'passwd can not be uname.';
+
+    else if (sha1($_GET['uname']) === sha1($_POST['passwd'])&($_GET['id']=='galaxy'))
+
+        die('Flag: '.$flag);
+
+    else
+
+        print 'sorry!';
+
+}
+?> 
